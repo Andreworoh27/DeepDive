19dd8d7cf0e92b67aa86c7f51140ddfc927908a4
Andrew Oroh <andreworoh.ao@gmail.com>
2024-04-26 11:40:51 +0700

    Add Initial Project

---
+++ DeepDive/DeepDive/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeepDive
+//
+//  Created by Andrew Oroh on 26/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DeepDive/DeepDive/DeepDiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepDiveApp.swift
+//  DeepDive
+//
+//  Created by Andrew Oroh on 26/04/24.
+//
+
+import SwiftUI
+
+@main
+struct DeepDiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
